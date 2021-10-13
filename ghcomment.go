74c2228c521912a3bb6a53a059d3f00fb363cebe
Viktor Voltaire <viktor.voltaire@einride.tech>
2021-10-13 15:54:06 +0200

    feat: add terraform and ghcomment tools

---
+++ ghcomment.go
@@ -0,0 +1,43 @@
+package tools
+
+import (
+	"fmt"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+func GhReviewTerraformPlan(prNumber string, gcpProject string) {
+	terraformPlanFile := "terraform.plan"
+	mg.Deps(
+		Terraform,
+		GHComment,
+		mg.F(FileExist, terraformPlanFile),
+	)
+
+	comment, _ := sh.Output(
+		"terraform",
+		"show",
+		"-no-color",
+		terraformPlanFile,
+	)
+	comment = fmt.Sprintf("```"+"hcl\n%s\n"+"```", comment)
+	ghComment := fmt.Sprintf(`
+<div>
+<img align="right" width="120" src="https://www.terraform.io/assets/images/logo-text-8c3ba8a6.svg" />
+<h2>Terraform Plan (%s)</h2>
+</div>
+
+%s
+`, gcpProject, comment)
+
+	out, _ := sh.Output(
+		"ghcomment",
+		"--pr",
+		prNumber,
+		"--signkey",
+		gcpProject,
+		"--comment",
+		ghComment,
+	)
+	fmt.Println(out)
+}
