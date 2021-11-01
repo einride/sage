d9850f53178b0dfc78620c64185531a2311bf3da
Viktor Voltaire <viktor.voltaire@einride.tech>
2021-11-01 16:57:15 +0100

    feat: move target tools into tools package

--- .mage/magefile.go
+++ .mage/magefile.go
@@ -6,9 +6,7 @@ import (
 	// mage:import
 	_ "github.com/einride/mage-tools/make"
 	// mage:import
-	_ "github.com/einride/mage-tools/golangci_lint"
-	// mage:import
-	_ "github.com/einride/mage-tools/goreview"
+	_ "github.com/einride/mage-tools/tools/golangci_lint"
 	"github.com/einride/mage-tools/tools"
 )
 
