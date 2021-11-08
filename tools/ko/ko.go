89f991fbf7f058b5d700b8548fb82a0730dcc8e8
Viktor Voltaire <viktor.voltaire@einride.tech>
2021-11-08 19:53:33 +0100

    feat: add ko targets

---
+++ tools/ko/ko.go
@@ -0,0 +1,77 @@
+package ko
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/einride/mage-tools/tools"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+func PublishLocal() error {
+	dockerTag, err := tag()
+	if err != nil {
+		return err
+	}
+	err = publish(
+		[]string{
+			"publish",
+			"--local",
+			"--preserve-import-paths",
+			"-t",
+			dockerTag,
+			"./cmd/server",
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Publish(repo string) error {
+	_ = os.Setenv("KO_DOCKER_REPO", repo)
+	dockerTag, err := tag()
+	if err != nil {
+		return err
+	}
+	err = publish(
+		[]string{
+			"publish",
+			"--preserve-import-paths",
+			"-t",
+			dockerTag,
+			"./cmd/server",
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func tag() (string, error) {
+	revision, err := sh.Output("git", "rev-parse", "--verify", "HEAD")
+	if err != nil {
+		return "", err
+	}
+	diff, err := sh.Output("git", "status", "--porcelain")
+	if err != nil {
+		return "", err
+	}
+	if diff != "" {
+		revision += "-dirty"
+	}
+	_ = os.Setenv("DOCKER_TAG", revision)
+	return revision, nil
+}
+
+func publish(args []string) error {
+	mg.Deps(tools.Ko)
+	fmt.Println("[ko] info building ko...")
+	if err := sh.RunV("ko", args...); err != nil {
+		return err
+	}
+	return nil
+}
