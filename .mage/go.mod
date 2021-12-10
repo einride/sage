83a328f0e3569a288c7db9dc91e7c116a34d5b8a
Viktor Voltaire <viktor.voltaire@einride.tech>
2021-12-10 19:22:56 +0100

    chore: add all target

--- .mage/go.mod
+++ .mage/go.mod
@@ -2,7 +2,10 @@ module github.com/einride/mage-tools/.mage
 
 go 1.17
 
-require github.com/einride/mage-tools v0.0.0-00010101000000-000000000000
+require (
+	github.com/einride/mage-tools v0.0.0-00010101000000-000000000000
+	github.com/magefile/mage v1.11.1-0.20210928193738-dd944245e253
+)
 
 require (
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -11,7 +14,6 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/iancoleman/strcase v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/magefile/mage v1.11.1-0.20210928193738-dd944245e253 // indirect
 	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa // indirect
 	golang.org/x/sys v0.0.0-20211109184856-51b60fd695b3 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- .mage/magefile.go
+++ .mage/magefile.go
@@ -3,15 +3,32 @@
 package main
 
 import (
-	"github.com/einride/mage-tools/tools"
+	"github.com/magefile/mage/mg"
+
 	// mage:import
 	_ "github.com/einride/mage-tools/maketargets"
 	// mage:import
-	_ "github.com/einride/mage-tools/tools/golangcilint"
+	"github.com/einride/mage-tools/tools/goreview"
+	// mage:import
+	"github.com/einride/mage-tools/tools/golangcilint"
+	// mage:import
+	"github.com/einride/mage-tools/tools/commitlint"
 	// mage:import semantic-release
 	_ "github.com/einride/mage-tools/tools/semanticrelease"
+	// mage:import
+	"github.com/einride/mage-tools/tools/gitverifynodiff"
+	// mage:import
+	"github.com/einride/mage-tools/tools/common"
 )
 
-func init() {
-	tools.Path = ".tools"
+func All() {
+	mg.Deps(
+		mg.F(commitlint.Commitlint, "main"),
+		golangcilint.GolangciLint,
+		goreview.Goreview,
+	)
+	mg.SerialDeps(
+		common.GoModTidy,
+		gitverifynodiff.GitVerifyNoDiff,
+	)
 }
