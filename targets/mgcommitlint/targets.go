8f47722d697deda3a1005b1bd7caa53515870ce3
Filip Tammergård <filip@tammergard.se>
2021-12-22 15:01:55 +0100

    feat: ignore dependabot messages instead of treating everything as warning

--- targets/mgcommitlint/targets.go
+++ targets/mgcommitlint/targets.go
@@ -21,11 +21,10 @@ const packageJSONContent = `{
 
 const commitlintFileContent = `module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    // Treat as warning until Dependabot supports commitlint.
-    // https://github.com/dependabot/dependabot-core/issues/2445
-    "body-max-line-length": [1, "always", 100],
-  }
+  ignores: [
+    // ignore dependabot messages
+    (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
+  ],
 };`
 
 var executable string
