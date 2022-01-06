dbdda2c5773d009162b515f66855243373de1f44
Oscar Söderlund <odsod@users.noreply.github.com>
2022-01-06 16:13:14 +0100

    feat(mggo): write Go test coverage profile

--- targets/mggo/targets.go
+++ targets/mggo/targets.go
@@ -1,16 +1,34 @@
 package mggo
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/magefile/mage/sh"
 	"go.einride.tech/mage-tools/mglog"
+	"go.einride.tech/mage-tools/mgtool"
 )
 
 func GoTest() error {
-	mglog.Logger("go-test").Info("running unit tests..")
-	return sh.RunV("go", "test", "-race", "-cover", "./...")
+	const toolName = "go"
+	mglog.Logger("go-test").Info("running Go unit tests..")
+	coverFile := filepath.Join(mgtool.GetPath(), toolName, "go-test.txt")
+	if err := os.MkdirAll(filepath.Dir(coverFile), 0o755); err != nil {
+		return err
+	}
+	return sh.RunV(
+		"go",
+		"test",
+		"-race",
+		"-coverprofile",
+		coverFile,
+		"-covermode",
+		"atomic",
+		"./...",
+	)
 }
 
 func GoModTidy() error {
-	mglog.Logger("go-mod-tidy").Info("tidying up mod file...")
+	mglog.Logger("go-mod-tidy").Info("tidying Go module files...")
 	return sh.RunV("go", "mod", "tidy", "-v")
 }
