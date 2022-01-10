79d3ea3f0f7880e05f6fa194b18cd9ef99cf7530
Emil Edholm <emil.edholm@einride.tech>
2022-01-10 11:28:01 +0100

    feat(hadolint): add Dockerfile linter
    See https://github.com/hadolint/hadolint for more information.
    
    No default config has been specified since I don't think we are using
    a custom default config right now. Can be added in the future if needed.

---
+++ targets/mghadolint/targets.go
@@ -0,0 +1,68 @@
+// Package mghadolint is a Dockerfile linter that you can employ to check for Dockerfile best
+// practices and common mistakes.
+package mghadolint
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/go-logr/logr"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+	"go.einride.tech/mage-tools/mglog"
+	"go.einride.tech/mage-tools/mgtool"
+)
+
+// version can be found here: https://github.com/hadolint/hadolint
+const version = "2.8.0"
+
+// nolint: gochecknoglobals
+var executable string
+
+func Hadolint(ctx context.Context) error {
+	ctx = logr.NewContext(ctx, mglog.Logger("hadolint"))
+	mg.CtxDeps(ctx, mg.F(prepare))
+	logr.FromContextOrDiscard(ctx).Info("running...")
+	dockerfilesRaw, err := sh.Output("git", "ls-files", "--exclude-standard", "--cached", "--others", "--", "*Dockerfile*")
+	if err != nil {
+		return fmt.Errorf("failed to list Dockerfiles: %w", err)
+	}
+	if dockerfilesRaw == "" {
+		// No Dockerfiles to lint, then there is no need to run hadolint.
+		return nil
+	}
+	dockerfiles := strings.Split(dockerfilesRaw, "\n")
+	return sh.RunV(executable, dockerfiles...)
+}
+
+func prepare(ctx context.Context) error {
+	const binaryName = "hadolint"
+	toolDir := filepath.Join(mgtool.GetPath(), binaryName)
+	binDir := filepath.Join(toolDir, version, "bin")
+	binary := filepath.Join(binDir, binaryName)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	if hostArch == mgtool.AMD64 {
+		hostArch = mgtool.X8664
+	}
+	hadolint := fmt.Sprintf("hadolint-%s-%s", strings.ToTitle(hostOS), hostArch)
+	binURL := fmt.Sprintf(
+		"https://github.com/hadolint/hadolint/releases/download/v%s/%s",
+		version,
+		hadolint,
+	)
+	if err := mgtool.FromRemote(
+		ctx,
+		binURL,
+		mgtool.WithDestinationDir(binDir),
+		mgtool.WithRenameFile(fmt.Sprintf("%s/hadolint", hadolint), binaryName),
+		mgtool.WithSkipIfFileExists(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", binaryName, err)
+	}
+	executable = binary
+	return nil
+}
