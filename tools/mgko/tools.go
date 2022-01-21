85d5982d9ca6d38262fc2e4dd93939dc225e4c8f
Oscar Söderlund <odsod@users.noreply.github.com>
2022-01-21 08:23:13 +0100

    feat(mgko): reduce API surface
    Not all applications have the buildable artifact in `cmd/server`, so
    these commands are not re-usable enough, but the git tag computation can
    be turned into convention and has been moved to `mggit`.

--- tools/mgko/tools.go
+++ tools/mgko/tools.go
@@ -1,14 +1,11 @@
 package mgko
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 
 	"github.com/go-logr/logr"
 	"github.com/magefile/mage/mg"
@@ -28,76 +25,19 @@ func Command(ctx context.Context, args ...string) *exec.Cmd {
 	return mgtool.Command(commandPath, args...)
 }
 
-func PublishLocalCommand(ctx context.Context) *exec.Cmd {
-	dockerTag, err := tag()
-	if err != nil {
-		panic(err)
-	}
-	return Command(
-		ctx,
-		"publish",
-		"--local",
-		"--preserve-import-paths",
-		"-t",
-		dockerTag,
-		"./cmd/server",
-	)
-}
-
-func PublishCommand(ctx context.Context, repo string) *exec.Cmd {
-	_ = os.Setenv("KO_DOCKER_REPO", repo)
-	dockerTag, err := tag()
-	if err != nil {
-		panic(err)
-	}
-	return Command(
-		ctx,
-		"publish",
-		"--preserve-import-paths",
-		"-t",
-		dockerTag,
-		"./cmd/server",
-	)
-}
-
-func tag() (string, error) {
-	cmd := mgtool.Command("git", "rev-parse", "--verify", "HEAD")
-	var b bytes.Buffer
-	cmd.Stdout = &b
-	if err := cmd.Run(); err != nil {
-		return "", err
-	}
-	revision := strings.TrimSpace(b.String())
-	cmd = mgtool.Command("git", "status", "--porcelain")
-	var diff bytes.Buffer
-	cmd.Stdout = &diff
-	if err := cmd.Run(); err != nil {
-		return "", err
-	}
-	if diff.String() != "" {
-		revision += "-dirty"
-	}
-	_ = os.Setenv("DOCKER_TAG", revision)
-	return revision, nil
-}
-
 type Prepare mgtool.Prepare
 
 func (Prepare) Ko(ctx context.Context) error {
 	const binaryName = "ko"
-
 	hostOS := runtime.GOOS
-
 	binDir := mgpath.FromTools(binaryName, version, "bin")
 	binary := filepath.Join(binDir, binaryName)
-
 	binURL := fmt.Sprintf(
 		"https://github.com/google/ko/releases/download/v%s/ko_%s_%s_x86_64.tar.gz",
 		version,
 		version,
 		hostOS,
 	)
-
 	if err := mgtool.FromRemote(
 		ctx,
 		binURL,
@@ -108,7 +48,6 @@ func (Prepare) Ko(ctx context.Context) error {
 	); err != nil {
 		return fmt.Errorf("unable to download %s: %w", binaryName, err)
 	}
-
 	commandPath = binary
 	return nil
 }
