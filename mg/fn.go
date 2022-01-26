84b7c2905324c58d6ad30610a93eecdf55cf88d3
Oscar Söderlund <odsod@users.noreply.github.com>
2022-01-26 09:20:09 +0100

    chore: simplify fn.go

--- mg/fn.go
+++ mg/fn.go
@@ -22,7 +22,7 @@ type Function interface {
 // to F, even if the target takes a context. Compatible args are int, bool, string, and
 // time.Duration.
 func F(target interface{}, args ...interface{}) Function {
-	hasContext, isNamespace, err := checkF(target, args)
+	hasNamespace, err := checkF(target, args)
 	if err != nil {
 		panic(err)
 	}
@@ -30,23 +30,18 @@ func F(target interface{}, args ...interface{}) Function {
 		name: runtime.FuncForPC(reflect.ValueOf(target).Pointer()).Name(),
 		f: func(ctx context.Context) error {
 			v := reflect.ValueOf(target)
-			count := len(args)
-			if hasContext {
-				count++
-			}
-			if isNamespace {
+			count := len(args) + 1
+			if hasNamespace {
 				count++
 			}
 			vargs := make([]reflect.Value, count)
 			x := 0
-			if isNamespace {
+			if hasNamespace {
 				vargs[0] = reflect.ValueOf(struct{}{})
 				x++
 			}
-			if hasContext {
-				vargs[x] = reflect.ValueOf(ctx)
-				x++
-			}
+			vargs[x] = reflect.ValueOf(ctx)
+			x++
 			for y := range args {
 				vargs[x+y] = reflect.ValueOf(args[y])
 			}
@@ -78,29 +73,28 @@ func (f fn) Run(ctx context.Context) error {
 	return f.f(ctx)
 }
 
-func checkF(target interface{}, args []interface{}) (hasContext, isNamespace bool, _ error) {
+func checkF(target interface{}, args []interface{}) (hasNamespace bool, _ error) {
 	t := reflect.TypeOf(target)
 	if t == nil || t.Kind() != reflect.Func {
-		return false, false, fmt.Errorf("non-function passed to mg.F: %T", target)
+		return false, fmt.Errorf("non-function passed to mg.F: %T", target)
 	}
 	if t.NumOut() > 1 {
-		return false, false, fmt.Errorf("target has too many return values, must be zero or just an error: %T", target)
+		return false, fmt.Errorf("target has too many return values, must be zero or just an error: %T", target)
 	}
 	if t.NumOut() == 1 && t.Out(0) != reflect.TypeOf(func() error { return nil }).Out(0) {
-		return false, false, fmt.Errorf("target's return value is not an error")
+		return false, fmt.Errorf("target's return value is not an error")
 	}
 	// more inputs than slots is always an error
 	if len(args) > t.NumIn() {
-		return false, false, fmt.Errorf("too many arguments for target, got %d for %T", len(args), target)
+		return false, fmt.Errorf("too many arguments for target, got %d for %T", len(args), target)
 	}
 	if t.NumIn() == 0 {
-		return false, false, nil
+		return false, nil
 	}
 	x := 0
 	inputs := t.NumIn()
 	if t.In(0).AssignableTo(reflect.TypeOf(struct{}{})) {
-		// nameSpace func
-		isNamespace = true
+		hasNamespace = true
 		x++
 		// callers must leave off the namespace value
 		inputs--
@@ -108,14 +102,14 @@ func checkF(target interface{}, args []interface{}) (hasContext, isNamespace boo
 	if t.NumIn() > x && t.In(x) == reflect.TypeOf(func(context.Context) {}).In(0) {
 		// callers must leave off the context
 		inputs--
-		// let the upper function know it should pass us a context.
-		hasContext = true
 		// skip checking the first argument in the below loop if it's a context, since first arg is
 		// special.
 		x++
+	} else {
+		return false, fmt.Errorf("invalid function, must have context.Context as first argument")
 	}
 	if len(args) != inputs {
-		return false, false, fmt.Errorf("wrong number of arguments for target, got %d for %T", len(args), target)
+		return false, fmt.Errorf("wrong number of arguments for target, got %d for %T", len(args), target)
 	}
 	for _, arg := range args {
 		argT := t.In(x)
@@ -123,13 +117,13 @@ func checkF(target interface{}, args []interface{}) (hasContext, isNamespace boo
 		case reflect.TypeOf(0), reflect.TypeOf(""), reflect.TypeOf(false):
 			// ok
 		default:
-			return false, false, fmt.Errorf("argument %d (%s), is not a supported argument type", x, argT)
+			return false, fmt.Errorf("argument %d (%s), is not a supported argument type", x, argT)
 		}
 		passedT := reflect.TypeOf(arg)
 		if argT != passedT {
-			return false, false, fmt.Errorf("argument %d expected to be %s, but is %s", x, argT, passedT)
+			return false, fmt.Errorf("argument %d expected to be %s, but is %s", x, argT, passedT)
 		}
 		x++
 	}
-	return hasContext, isNamespace, nil
+	return hasNamespace, nil
 }
