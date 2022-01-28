dd7e82b84364132b1ee8689c49dec80d511b689d
Oscar Söderlund <odsod@users.noreply.github.com>
2022-01-28 13:12:03 +0100

    feat(gh): add tool

---
+++ tools/sggh/tools.go
@@ -0,0 +1,48 @@
+package sggh
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	name    = "gh"
+	version = "2.4.0"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(name), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	binDir := sg.FromToolsDir(name, version)
+	binary := filepath.Join(binDir, name)
+	binURL := fmt.Sprintf(
+		"https://github.com/cli/cli/releases/download/v%s/gh_%s_%s_%s.tar.gz",
+		version,
+		version,
+		hostOS,
+		hostArch,
+	)
+	if err := sgtool.FromRemote(
+		ctx,
+		binURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntarGz(),
+		sgtool.WithRenameFile(fmt.Sprintf("gh_%s_%s_%s/bin/gh", version, hostOS, hostArch), name),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", name, err)
+	}
+	return nil
+}
