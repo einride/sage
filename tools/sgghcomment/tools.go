a29d349ac1354af820b0ebd5e2a2568757b68bd2
Oscar Söderlund <odsod@users.noreply.github.com>
2022-01-28 12:49:46 +0100

    feat(ghcomment): add tool

---
+++ tools/sgghcomment/tools.go
@@ -0,0 +1,48 @@
+package sgghcomment
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	version    = "0.3.1"
+	binaryName = "ghcomment"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(binaryName), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	binDir := sg.FromToolsDir(binaryName, version)
+	binary := filepath.Join(binDir, binaryName)
+	if err := sgtool.FromRemote(
+		ctx,
+		fmt.Sprintf(
+			"https://github.com/einride/ghcomment/releases/download/v%s/ghcomment_%s_%s_%s.tar.gz",
+			version,
+			version,
+			runtime.GOOS,
+			runtime.GOARCH,
+		),
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntarGz(),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", binaryName, err)
+	}
+	if err := os.Chmod(binary, 0o755); err != nil {
+		return fmt.Errorf("unable to make %s command: %w", binaryName, err)
+	}
+	return nil
+}
