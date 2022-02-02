72df57e933ddf07d29d334e3b6d9c7a81b9f3b34
Oscar Söderlund <odsod@users.noreply.github.com>
2022-02-02 21:40:52 +0100

    feat(protoc-gen-go-aip-test): add package

---
+++ tools/sgprotocgengoaiptest/tools.go
@@ -0,0 +1,55 @@
+package sgprotocgengoaiptest
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	version = "0.4.4"
+	name    = "protoc-gen-go-aip-test"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(name), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	binDir := sg.FromToolsDir(name, version)
+	binary := filepath.Join(binDir, name)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	if hostArch == sgtool.AMD64 {
+		hostArch = sgtool.X8664
+	}
+	binURL := fmt.Sprintf(
+		"https://github.com/einride/protoc-gen-go-aip-test"+
+			"/releases/download/v%s/protoc-gen-go-aip-test_%s_%s_%s.tar.gz",
+		version,
+		version,
+		hostOS,
+		hostArch,
+	)
+	if err := sgtool.FromRemote(
+		ctx,
+		binURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntarGz(),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", name, err)
+	}
+	if err := os.Chmod(binary, 0o755); err != nil {
+		return fmt.Errorf("unable to make %s command: %w", name, err)
+	}
+	return nil
+}
