c5c5a29dde932023560adaab7ebc899f72e4bd67
Johan Stenehall <stenehall@gmail.com>
2022-02-05 00:14:19 +0100

    feat(protobuf-gen-http): add a protobuf-http-generator

---
+++ tools/sgprotocgentypescriptaip/tools.go
@@ -0,0 +1,57 @@
+package sgprotocgentypescriptaip
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	version    = "0.1.2"
+	binaryName = "protoc-gen-typescript-aip"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(binaryName), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	binDir := sg.FromToolsDir(binaryName, version)
+	binary := filepath.Join(binDir, binaryName)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	if hostArch == sgtool.AMD64 {
+		hostArch = sgtool.X8664
+	}
+	// nolint: lll
+	downloadURL := fmt.Sprintf(
+		"https://github.com/einride/protoc-gen-typescript-aip/releases/download/v%s/protoc-gen-typescript-aip_%s_%s_%s.tar.gz",
+		version,
+		version,
+		hostOS,
+		hostArch,
+	)
+
+	if err := sgtool.FromRemote(
+		ctx,
+		downloadURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntarGz(),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", binaryName, err)
+	}
+	if err := os.Chmod(binary, 0o755); err != nil {
+		return fmt.Errorf("unable to make %s command: %w", binaryName, err)
+	}
+
+	return nil
+}
---
+++ tools/sgprotocgentypescripthttp/tools.go
@@ -0,0 +1,57 @@
+package sgprotocgentypescripthttp
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	version    = "0.7.0"
+	binaryName = "protoc-gen-typescript-http"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(binaryName), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	binDir := sg.FromToolsDir(binaryName, version)
+	binary := filepath.Join(binDir, binaryName)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	if hostArch == sgtool.AMD64 {
+		hostArch = sgtool.X8664
+	}
+	// nolint: lll
+	downloadURL := fmt.Sprintf(
+		"https://github.com/einride/protoc-gen-typescript-http/releases/download/v%s/protoc-gen-typescript-http_%s_%s_%s.tar.gz",
+		version,
+		version,
+		hostOS,
+		hostArch,
+	)
+
+	if err := sgtool.FromRemote(
+		ctx,
+		downloadURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntarGz(),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", binaryName, err)
+	}
+	if err := os.Chmod(binary, 0o755); err != nil {
+		return fmt.Errorf("unable to make %s command: %w", binaryName, err)
+	}
+
+	return nil
+}
