4fa73d70ffcdd8e1c30f7f37317d257b33e4d53d
Johan Stenehall <stenehall@gmail.com>
2022-02-04 15:33:36 +0100

    chore(sggo): add explanation for the TestCommand
    This adds an explanation for the command and breaks out the
    PrepareCommand parts of the command.

--- tools/sggo/tools.go
+++ tools/sggo/tools.go
@@ -9,6 +9,7 @@ import (
 	"go.einride.tech/sage/sg"
 )
 
+// TestCommand runs the default go test command and generates a coverage report.
 func TestCommand(ctx context.Context) *exec.Cmd {
 	coverFile := sg.FromBuildDir("go", "coverage", "go-test.txt")
 	if err := os.MkdirAll(filepath.Dir(coverFile), 0o755); err != nil {
