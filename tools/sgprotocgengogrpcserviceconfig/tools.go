a9ee1923474b69d9a4fe60314ac2b29e07bc2c72
Viktor Voltaire <viktor.voltaire@einride.tech>
2022-02-07 19:47:40 +0100

    feat(tools): add protoc-gen-go-grpc-service-config

---
+++ tools/sgprotocgengogrpcserviceconfig/tools.go
@@ -0,0 +1,52 @@
+package sgprotocgengogrpc
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	version = "0.2.0"
+	name    = "protoc-gen-go-grpc-service-config"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(name), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	binDir := sg.FromToolsDir(name, version)
+	binary := filepath.Join(binDir, name)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	// nolint: lll
+	binURL := fmt.Sprintf(
+		"https://github.com/einride/protoc-gen-go-grpc-service-config/releases/download/v%s/protoc-gen-go-grpc-service-config_%s_%s_%s.tar.gz",
+		version,
+		version,
+		hostOS,
+		hostArch,
+	)
+	if err := sgtool.FromRemote(
+		ctx,
+		binURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntarGz(),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", name, err)
+	}
+	if err := os.Chmod(binary, 0o755); err != nil {
+		return fmt.Errorf("unable to make %s command: %w", name, err)
+	}
+	return nil
+}
