1ffc846220349a95bfca787df1aa69debecaef0f
Viktor Voltaire <viktor.voltaire@einride.tech>
2022-02-09 15:08:16 +0100

    feat(sggit): remove Version and introduce SHA and ShortSHA

--- tools/sggit/tools.go
+++ tools/sggit/tools.go
@@ -24,15 +24,28 @@ func VerifyNoDiff(ctx context.Context) error {
 	return nil
 }
 
-func Version(ctx context.Context) string {
+func SHA(ctx context.Context) string {
 	revision := sg.Output(
-		sg.Command(ctx, "git", "rev-parse", "--verify", "--short", "HEAD"),
+		sg.Command(ctx, "git", "rev-parse", "--verify", "HEAD"),
 	)
-	diff := sg.Output(
-		sg.Command(ctx, "git", "status", "--porcelain"),
-	)
-	if diff != "" {
+	if diff(ctx) != "" {
 		revision += "-dirty"
 	}
 	return revision
+}
+
+func ShortSHA(ctx context.Context) string {
+	revision := sg.Output(
+		sg.Command(ctx, "git", "rev-parse", "--verify", "--short", "HEAD"),
+	)
+	if diff(ctx) != "" {
+		revision += "-dirty"
+	}
+	return revision
+}
+
+func diff(ctx context.Context) string {
+	return sg.Output(
+		sg.Command(ctx, "git", "status", "--porcelain"),
+	)
 }
