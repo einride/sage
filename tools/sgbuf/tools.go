8fb207997b4160415d6dc7073e2a545039dfc338
Oscar Söderlund <odsod@users.noreply.github.com>
2022-02-21 19:29:21 +0100

    fix(buf): fix updated binary path

--- tools/sgbuf/tools.go
+++ tools/sgbuf/tools.go
@@ -6,6 +6,8 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"unicode"
+	"unicode/utf8"
 
 	"go.einride.tech/sage/sg"
 	"go.einride.tech/sage/sgtool"
@@ -22,28 +24,35 @@ func Command(ctx context.Context, args ...string) *exec.Cmd {
 }
 
 func PrepareCommand(ctx context.Context) error {
-	binDir := sg.FromToolsDir(name, version, "bin")
-	binary := filepath.Join(binDir, name)
-	hostOS := runtime.GOOS
-	hostArch := runtime.GOARCH
-	if hostArch == sgtool.AMD64 {
-		hostArch = sgtool.X8664
+	toolDir := sg.FromToolsDir(name, version)
+	binary := filepath.Join(toolDir, name, "bin", name)
+	arch := runtime.GOARCH
+	if arch == sgtool.AMD64 {
+		arch = sgtool.X8664
 	}
 	binURL := fmt.Sprintf(
-		"https://github.com/bufbuild/buf/releases/download/v%s/buf-%s-%s",
+		"https://github.com/bufbuild/buf/releases/download/v%s/buf-%s-%s.tar.gz",
 		version,
-		hostOS,
-		hostArch,
+		toInitialCamel(runtime.GOOS),
+		arch,
 	)
 	if err := sgtool.FromRemote(
 		ctx,
 		binURL,
-		sgtool.WithDestinationDir(binDir),
-		sgtool.WithRenameFile("", name),
+		sgtool.WithDestinationDir(toolDir),
+		sgtool.WithUntarGz(),
 		sgtool.WithSkipIfFileExists(binary),
 		sgtool.WithSymlink(binary),
 	); err != nil {
 		return fmt.Errorf("unable to download %s: %w", name, err)
 	}
 	return nil
+}
+
+func toInitialCamel(s string) string {
+	r, n := utf8.DecodeRuneInString(s)
+	if r == utf8.RuneError {
+		return s
+	}
+	return string(unicode.ToUpper(r)) + s[n:]
 }
