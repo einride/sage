378c24e8dab78fff51ed7a2bcd640be433f998f7
Douglas Wayne Potter <douglas.potter@einride.tech>
2022-04-19 14:30:54 +0200

    feat: add jsonfmt to tools

---
+++ tools/sgjsonfmt/tools.go
@@ -0,0 +1,56 @@
+package sgjsonfmt
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	version    = "0.5.0"
+	binaryName = "jsonfmt"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(binaryName), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	binDir := sg.FromToolsDir(binaryName, version)
+	binary := filepath.Join(binDir, binaryName)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	if hostOS == sgtool.Darwin {
+		hostArch = "all"
+	}
+
+	binURL := fmt.Sprintf(
+		"https://github.com/caarlos0/jsonfmt/"+
+			"releases/download/v%s/jsonfmt_%s_%s_%s.tar.gz",
+		version,
+		version,
+		hostOS,
+		hostArch,
+	)
+	if err := sgtool.FromRemote(
+		ctx,
+		binURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntarGz(),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", binaryName, err)
+	}
+	if err := os.Chmod(binary, 0o755); err != nil {
+		return fmt.Errorf("unable to make %s command: %w", binaryName, err)
+	}
+	return nil
+}
