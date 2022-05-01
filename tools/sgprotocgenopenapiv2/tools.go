e522b9d67c21a47eac72ae341ce5b8145b4a2bd8
Oscar Söderlund <odsod@users.noreply.github.com>
2022-05-01 14:43:47 +0200

    feat(sgprotocgenopenapiv2): add tool
    Generates OpenAPIv2 specs from gRPC services.

---
+++ tools/sgprotocgenopenapiv2/tools.go
@@ -0,0 +1,55 @@
+package sgprotocgenopenapiv2
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	version = "2.10.0"
+	name    = "protoc-gen-openapiv2"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(name), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	binDir := sg.FromToolsDir(name, version)
+	binary := filepath.Join(binDir, name)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	if hostArch == sgtool.AMD64 {
+		hostArch = sgtool.X8664
+	}
+	binURL := fmt.Sprintf(
+		"https://github.com/grpc-ecosystem/grpc-gateway/releases/download/v%s/%s-v%s-%s-%s",
+		version,
+		name,
+		version,
+		hostOS,
+		hostArch,
+	)
+	if err := sgtool.FromRemote(
+		ctx,
+		binURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithRenameFile("", name),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", name, err)
+	}
+	if err := os.Chmod(binary, 0o755); err != nil {
+		return fmt.Errorf("unable to make %s command: %w", name, err)
+	}
+	return nil
+}
