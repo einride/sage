b712eaad388e523290b74df3b676222d48325904
Oscar Söderlund <odsod@users.noreply.github.com>
2022-07-17 19:38:56 +0200

    feat(sgyq): add package
    The yq tool for working with YAML.

---
+++ tools/sgyq/tools.go
@@ -0,0 +1,47 @@
+package sgyq
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	name    = "yq"
+	version = "4.26.1"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(name), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	binDir := sg.FromToolsDir(name, version)
+	binary := filepath.Join(binDir, name)
+	binURL := fmt.Sprintf(
+		"https://github.com/mikefarah/yq/releases/download/v%s/yq_%s_%s.tar.gz",
+		version,
+		hostOS,
+		hostArch,
+	)
+	if err := sgtool.FromRemote(
+		ctx,
+		binURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntarGz(),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithRenameFile(fmt.Sprintf("./yq_%s_%s", hostOS, hostArch), name),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", name, err)
+	}
+	return nil
+}
