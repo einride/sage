d5cf1cf0e5009a86e2866a6fdcee0352ac3cac7e
Hashem Hashem <hashem.hashem@einride.tech>
2022-08-16 14:40:11 +0200

    feat(tools): add xz
    Needed to download tools that are packaged as xz archives.

---
+++ tools/sgxz/tools.go
@@ -0,0 +1,55 @@
+package sgxz
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	version = "5.2.5"
+	name    = "xz"
+)
+
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(name), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	const binaryName = "xz"
+	toolDir := sg.FromToolsDir(binaryName)
+	binDir := filepath.Join(toolDir, version, "bin")
+	binary := filepath.Join(binDir, binaryName)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	if hostArch == sgtool.AMD64 {
+		hostArch = sgtool.X8664
+	}
+	if hostOS == sgtool.Darwin && hostArch == sgtool.ARM64 {
+		hostArch = sgtool.X8664
+	}
+	xz := fmt.Sprintf("%s-%s-%s-%s", binaryName, version, hostOS, hostArch)
+	binURL := fmt.Sprintf(
+		"https://github.com/therootcompany/xz-static/releases/download/v%s/%s.tar.gz",
+		version,
+		xz,
+	)
+	if err := sgtool.FromRemote(
+		ctx,
+		binURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithUntar(),
+		sgtool.WithRenameFile(fmt.Sprintf("./%s/xz", xz), binaryName),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", binaryName, err)
+	}
+	return nil
+}
