4903fe55d5c977843dab5771247de6acf381b0fe
Oscar Söderlund <odsod@users.noreply.github.com>
2023-07-23 13:24:37 +0200

    feat(govulncheck): add helper for checking all modules in repo
    Including logging helper that ensures that the current module is a
    appended as a prefix to the log output.

--- tools/sggovulncheck/tools.go
+++ tools/sggovulncheck/tools.go
@@ -2,7 +2,10 @@ package sggovulncheck
 
 import (
 	"context"
+	"fmt"
+	"io/fs"
 	"os/exec"
+	"path/filepath"
 
 	"go.einride.tech/sage/sg"
 	"go.einride.tech/sage/sgtool"
@@ -10,19 +13,61 @@ import (
 
 const (
 	name    = "govulncheck"
-	version = "latest"
+	version = "v1.0.0"
 )
 
+// Command returns an [*exec.Cmd] for govulncheck.
 func Command(ctx context.Context, args ...string) *exec.Cmd {
 	sg.Deps(ctx, PrepareCommand)
 	return sg.Command(ctx, sg.FromBinDir(name), args...)
 }
 
+// RunAll runs govulncheck on all the specified module paths, or all module paths from the current git root by default.
+func RunAll(ctx context.Context, modulePaths ...string) error {
+	var commands []*exec.Cmd
+	if err := filepath.WalkDir(sg.FromGitRoot(), func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() || d.Name() != "go.mod" {
+			return nil
+		}
+		if len(modulePaths) > 0 {
+			var shouldRunInModulePath bool
+			for _, modulePath := range modulePaths {
+				if modulePath == path {
+					shouldRunInModulePath = true
+					break
+				}
+			}
+			if !shouldRunInModulePath {
+				return nil
+			}
+		}
+		relativePath, err := filepath.Rel(sg.FromGitRoot(), path)
+		if err != nil {
+			return err
+		}
+		cmd := Command(
+			sg.AppendLoggerPrefix(ctx, fmt.Sprintf(" (%s): ", relativePath)),
+			"-C",
+			filepath.Dir(path),
+			"./...",
+		)
+		commands = append(commands, cmd)
+		return cmd.Start()
+	}); err != nil {
+		return err
+	}
+	for _, cmd := range commands {
+		if err := cmd.Wait(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func PrepareCommand(ctx context.Context) error {
-	_, err := sgtool.GoInstall(
-		ctx,
-		"golang.org/x/vuln/cmd/govulncheck",
-		version,
-	)
+	_, err := sgtool.GoInstall(ctx, "golang.org/x/vuln/cmd/govulncheck", version)
 	return err
 }
