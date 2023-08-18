4807c274114949e6c74b9696227eb80dcdaead1d
João Veiga <joao.veiga@einride.tech>
2023-08-18 11:28:31 +0200

    feat(commit-lint): add build to ignored commit tags

--- tools/sgcommitlint/tools.go
+++ tools/sgcommitlint/tools.go
@@ -21,7 +21,7 @@ const commitlintFileContent = `module.exports = {
   extends: ["@commitlint/config-conventional"],
   ignores: [
     // ignore dependabot commits
-    (message) => /chore\(deps(-dev)?\): bump/.test(message),
+    (message) => /(chore|build)\(deps(-dev)?\): bump/.test(message),
   ],
 }`
 
