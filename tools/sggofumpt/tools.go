9c769a9b0e34338142b9cd0f3598c48a410edd01
Fredrik Averpil <fredrik.averpil@einride.tech>
2024-03-01 16:56:16 +0100

    feat(tools): add gofumpt

---
+++ tools/sggofumpt/tools.go
@@ -0,0 +1,53 @@
+package gofumpt
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	name    = "gofumpt"
+	version = "v0.6.0"
+)
+
+// Command returns an [*exec.Cmd] for golines.
+func Command(ctx context.Context, args ...string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	return sg.Command(ctx, sg.FromBinDir(name), args...)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	binDir := sg.FromToolsDir(name, version)
+	binary := filepath.Join(binDir, name)
+	hostOS := runtime.GOOS
+	hostArch := runtime.GOARCH
+	binURL := fmt.Sprintf(
+		"https://github.com/mvdan/gofumpt/"+
+			"releases/download/%s/gofumpt_%s_%s_%s",
+		version,
+		version,
+		hostOS,
+		hostArch,
+	)
+	if err := sgtool.FromRemote(
+		ctx,
+		binURL,
+		sgtool.WithDestinationDir(binDir),
+		sgtool.WithRenameFile(fmt.Sprintf("gofumpt_%s_%s_%s", version, hostOS, hostArch), name),
+		sgtool.WithSkipIfFileExists(binary),
+		sgtool.WithSymlink(binary),
+	); err != nil {
+		return fmt.Errorf("unable to download %s: %w", name, err)
+	}
+	if err := os.Chmod(binary, 0o755); err != nil {
+		return fmt.Errorf("unable to make %s command: %w", name, err)
+	}
+	return nil
+}
