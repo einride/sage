105961d5ee834912804d19498805ebb754a9d924
Oscar Söderlund <odsod@users.noreply.github.com>
2024-04-08 06:03:16 +0200

    feat(markdownfmt): mark as deprecated

--- tools/sgmarkdownfmt/tools.go
+++ tools/sgmarkdownfmt/tools.go
@@ -16,11 +16,17 @@ const version = "75134924a9fd3335f76a9709314c5f5cef4d6ddc"
 //nolint:gochecknoglobals
 var commandPath string
 
+// Command returns an [exec.Cmd] pointing to the markdownfmt binary.
+//
+// Deprecated: Use sgmdformat.Command instead.
 func Command(ctx context.Context, args ...string) *exec.Cmd {
 	sg.Deps(ctx, PrepareCommand)
 	return sg.Command(ctx, commandPath, args...)
 }
 
+// PrepareCommand downloads the markdownfmt binary and adds it to the PATH.
+//
+// Deprecated: Use sgmdformat.PrepareCommand instead.
 func PrepareCommand(ctx context.Context) error {
 	binary, err := sgtool.GoInstall(ctx, "github.com/shurcooL/markdownfmt", version)
 	if err != nil {
