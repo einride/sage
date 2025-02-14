0d084dce3341ca04d5312492e34ea11bd346ed60
João Veiga <joao.veiga@einride.tech>
2025-02-14 13:28:59 +0100

    feat: support gopls check

---
+++ tools/sggopls/tools.go
@@ -0,0 +1,32 @@
+package sggopls
+
+import (
+	"context"
+	"os/exec"
+
+	"go.einride.tech/sage/sg"
+	"go.einride.tech/sage/sgtool"
+)
+
+const (
+	name    = "gopls"
+	version = "v0.17.1"
+)
+
+// Check runs `gopls check <PATH1> <PATH2>`.
+// gopls only works with paths to go files.
+func Check(ctx context.Context, paths []string) *exec.Cmd {
+	sg.Deps(ctx, PrepareCommand)
+	args := []string{"check"}
+	args = append(args, paths...)
+	return sg.Command(
+		ctx,
+		name,
+		args...,
+	)
+}
+
+func PrepareCommand(ctx context.Context) error {
+	_, err := sgtool.GoInstall(ctx, "golang.org/x/tools/gopls", version)
+	return err
+}
