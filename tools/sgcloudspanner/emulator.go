588f41731bd600bbc479c68eed6efd49d825b0e6
Ludwig Nyberg <44864447+loodde@users.noreply.github.com>
2025-04-01 14:16:16 +0200

    feat: bump cloud-spanner-emulator to v1.5.29

--- tools/sgcloudspanner/emulator.go
+++ tools/sgcloudspanner/emulator.go
@@ -17,7 +17,7 @@ import (
 // https://console.cloud.google.com/gcr/images/cloud-spanner-emulator/global/emulator
 const (
 	url     = "gcr.io/cloud-spanner-emulator/emulator"
-	version = "1.5.24"
+	version = "1.5.29"
 	image   = url + ":" + version
 )
 
