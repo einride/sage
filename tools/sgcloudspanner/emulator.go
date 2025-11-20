fdef05c49a0366a6996222cd600e3a0a93b1789a
Ludwig Nyberg <44864447+loodde@users.noreply.github.com>
2025-11-20 14:32:43 +0100

    fix(sgcloudspanner): bump emulator to 1.5.45

--- tools/sgcloudspanner/emulator.go
+++ tools/sgcloudspanner/emulator.go
@@ -17,7 +17,7 @@ import (
 // https://console.cloud.google.com/gcr/images/cloud-spanner-emulator/global/emulator
 const (
 	url     = "gcr.io/cloud-spanner-emulator/emulator"
-	version = "1.5.29"
+	version = "1.5.45"
 	image   = url + ":" + version
 )
 
