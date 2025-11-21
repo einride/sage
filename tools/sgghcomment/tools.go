430628800112f9f649a2c24f8945c4b28f0e6984
Alexandre Thenorio <alexandre.thenorio@einride.tech>
2025-11-21 16:17:42 +0100

    feat: debugging ghcomment

--- tools/sgghcomment/tools.go
+++ tools/sgghcomment/tools.go
@@ -2,18 +2,14 @@ package sgghcomment
 
 import (
 	"context"
-	"fmt"
-	"os"
 	"os/exec"
-	"path/filepath"
-	"runtime"
 
 	"go.einride.tech/sage/sg"
 	"go.einride.tech/sage/sgtool"
 )
 
 const (
-	version    = "0.4.0"
+	version    = "226e3abf52137d2af2f5d92a03402fa1839add43"
 	binaryName = "ghcomment"
 )
 
@@ -23,26 +19,28 @@ func Command(ctx context.Context, args ...string) *exec.Cmd {
 }
 
 func PrepareCommand(ctx context.Context) error {
-	binDir := sg.FromToolsDir(binaryName, version)
-	binary := filepath.Join(binDir, binaryName)
-	if err := sgtool.FromRemote(
-		ctx,
-		fmt.Sprintf(
-			"https://github.com/einride/ghcomment/releases/download/v%s/ghcomment_%s_%s_%s.tar.gz",
-			version,
-			version,
-			runtime.GOOS,
-			runtime.GOARCH,
-		),
-		sgtool.WithDestinationDir(binDir),
-		sgtool.WithUntarGz(),
-		sgtool.WithSkipIfFileExists(binary),
-		sgtool.WithSymlink(binary),
-	); err != nil {
-		return fmt.Errorf("unable to download %s: %w", binaryName, err)
-	}
-	if err := os.Chmod(binary, 0o755); err != nil {
-		return fmt.Errorf("unable to make %s command: %w", binaryName, err)
-	}
-	return nil
+	_, err := sgtool.GoInstall(ctx, "github.com/einride/ghcomment", version)
+	return err
+	// binDir := sg.FromToolsDir(binaryName, version)
+	// binary := filepath.Join(binDir, binaryName)
+	// if err := sgtool.FromRemote(
+	// 	ctx,
+	// 	fmt.Sprintf(
+	// 		"https://github.com/einride/ghcomment/releases/download/v%s/ghcomment_%s_%s_%s.tar.gz",
+	// 		version,
+	// 		version,
+	// 		runtime.GOOS,
+	// 		runtime.GOARCH,
+	// 	),
+	// 	sgtool.WithDestinationDir(binDir),
+	// 	sgtool.WithUntarGz(),
+	// 	sgtool.WithSkipIfFileExists(binary),
+	// 	sgtool.WithSymlink(binary),
+	// ); err != nil {
+	// 	return fmt.Errorf("unable to download %s: %w", binaryName, err)
+	// }
+	// if err := os.Chmod(binary, 0o755); err != nil {
+	// 	return fmt.Errorf("unable to make %s command: %w", binaryName, err)
+	// }
+	// return nil
 }
