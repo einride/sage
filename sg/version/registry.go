c5e00c01ce010353fe02890dca4873125347a4e7
Fredrik Averpil <fredrik.averpil@einride.tech>
2025-11-27 09:34:37 +0100

    feat(sggolangcilintv2): add to version checker

--- sg/version/registry.go
+++ sg/version/registry.go
@@ -1,6 +1,9 @@
 package version
 
-import "go.einride.tech/sage/tools/sgbuf"
+import (
+	"go.einride.tech/sage/tools/sgbuf"
+	"go.einride.tech/sage/tools/sggolangcilintv2"
+)
 
 // Tools is the registry of versionable tools.
 // Tools are added incrementally as they export their metadata.
@@ -14,5 +17,13 @@ var Tools = []Tool{
 		SourceType:     SourceGitHub,
 		Repo:           sgbuf.Repo,
 		TagPattern:     sgbuf.TagPattern,
+	},
+	{
+		Name:           sggolangcilintv2.Name,
+		FilePath:       "tools/sggolangcilintv2/tools.go",
+		CurrentVersion: sggolangcilintv2.Version,
+		SourceType:     SourceGitHub,
+		Repo:           sggolangcilintv2.Repo,
+		TagPattern:     sggolangcilintv2.TagPattern,
 	},
 }
